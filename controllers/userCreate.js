62540746f8e0dc3132ff9f2d979cee68e2815ca2
Abhik Mondal <168610652+Abhik381@users.noreply.github.com>
2024-07-25 22:35:46 +0530

    Update userCreate.js

--- controllers/userCreate.js
+++ controllers/userCreate.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports.userCreate = async (req, res) => {
   try {
-    const { email, contact, password } = res.body;
+    const { email, contact, password } = red.body;
     const user = await userModel.findOne({ email });
 
     if (user) return res.status(400).json({ message: "User already exists" });
