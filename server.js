e767e2dce2498569d7b1736844e043e72291babf
Abhik Mondal <168610652+Abhik381@users.noreply.github.com>
2024-07-28 00:22:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-const debuger = require("debug")("development:express");
+const debuger = require("debug")("production:express");
 const cookiParser = require("cookie-parser");
 const userRouter = require("./routes/userRouter.js");
 const mongoose_connection = require("./config/mongoose-connection.js");
 require("dotenv").config();
 const cors = require("cors");
-
-const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json());
@@ -16,6 +14,6 @@ app.use(cookiParser());
 
 app.use("/users", userRouter);
 
-app.listen(port, () => {
-  debuger(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  debuger(`Server is running on port ${process.env.PORT}`);
 });
