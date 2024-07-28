4498b61802d97ad8501be53510db6f9cc12e9789
Abhik Mondal <168610652+Abhik381@users.noreply.github.com>
2024-07-28 13:00:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const debuger = require("debug")("production:express");
 const cookiParser = require("cookie-parser");
 const userRouter = require("./routes/userRouter.js");
+const mainRouter = require("./routes/mainRouter.js");
 const mongoose_connection = require("./config/mongoose-connection.js");
 require("dotenv").config();
 const cors = require("cors");
@@ -12,14 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookiParser());
 
-app.get("/", function(req,res){
-  res.json({
-    name: "Abhik Mondal"
-  })
-}
-
+app.use("/", mainRouter)
 app.use("/users", userRouter);
 
 app.listen(process.env.PORT, () => {
-  debuger(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
