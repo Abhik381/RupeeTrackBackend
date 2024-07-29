3d1c414ac35bfb5931626b8dae62916fd1828a9e
Abhik2005 <abhikmondal532@gmail.com>
2024-07-29 13:02:03 +0530

    two route create

---
+++ controllers/userExpenseseDataControllers.js
@@ -0,0 +1,23 @@
+const userModel = require("../models/usersModel");
+const jwt = require("jsonwebtoken");
+
+const userData = async (req,res)=>{
+    try {
+        const {token} = req.body;
+        const user = jwt.verify(token,process.env.JWT_KEY);
+        const userData = await userModel.findOne({email: user.email}).select("-password").populate("expenses");
+        res.status(201).json({
+            message: "User Find Succesfully.",
+            data: userData,
+            success: true
+        })
+    } catch (error) {
+        console.log(error.message || error);
+        res.status(500).json({
+            message: error.message || error,
+            error: true
+        })
+    }
+}
+
+module.exports = userData;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,8 @@ const userLogin = require("../controllers/userLoginControllers.js");
 const userLogout = require("../controllers/userLogoutControllers.js");
 const userIncome = require("../controllers/userIncomeControllers.js");
 const userExpenses = require("../controllers/userExpensesControllers.js");
-
+const userIncomeData = require("../controllers/userIncomeDataControllers.js");
+const userExpensesData = require("../controllers/userExpenseseDataControllers.js");
 
 const router = express.Router();
 
@@ -13,5 +14,7 @@ router.post("/login", userLogin);
 router.get("/logout", userLogout);
 router.post("/income", userIncome);
 router.post("/expenses", userExpenses);
+router.post("/userincomedata", userIncomeData)
+router.post("/userexpensesdata", userExpensesData)
 
 module.exports = router;
